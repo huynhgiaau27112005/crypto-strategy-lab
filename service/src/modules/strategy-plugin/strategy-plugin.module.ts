98131113810202765ba72500383a03f112b8df41
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2026-08-12 23:57:31 +0700

    feat: add MAStrategy and BollingerStrategy providers

--- service/src/modules/strategy-plugin/strategy-plugin.module.ts
+++ service/src/modules/strategy-plugin/strategy-plugin.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { StrategyPluginController } from './strategy-plugin.controller';
 import { StrategyPluginService } from './strategy-plugin.service';
 import { registryProvider } from './config';
+import { MAStrategy } from './implementations/ma.strategy';
+import { BollingerStrategy } from './implementations/bollinger.strategy';
 
 @Module({
   controllers: [StrategyPluginController],
-  providers: [StrategyPluginService, registryProvider],
+  providers: [
+    StrategyPluginService,
+    MAStrategy,
+    BollingerStrategy,
+    registryProvider,
+  ],
   exports: [StrategyPluginService],
 })
 export class StrategyPluginModule {}
